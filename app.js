f7306c6e81d313e8dd2b0ab455d6fc1b7ee52754
Nasa Nguyen <nasa8x@gmail.com>
2018-09-07 23:11:59 +0700

    multiple masternodes

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var SmartRPC = require('smart-rpc'),
     CronJob = require('cron').CronJob,
     config = require('./config');
 
-var client = SmartRPC.create(config.coin);
 
-new CronJob('0 */1 * * * *', function () {   
-    client.sentinelPing(config.version).then((val) => {
-        console.log('%s ping %s', new Date(), val);
-    });
+
+new CronJob('0 */5 * * * *', function () {
+
+    for (let i = 0; i < config.coins.length; i++) {
+        setTimeout(function () {
+            const o = config.coins[i];
+            var client = SmartRPC.create(o);
+            client.sentinelPing(config.version).then((val) => {
+                console.log(o.id, new Date(), val);
+            });
+        }, i * 2000);
+
+    }
+
+
 }, null, true);
--- config.js
+++ config.js
@@ -1,16 +1,30 @@
 module.exports = {
 
-    version:'1.2.1',
-    coin: {
-        id: 'GEEK',
-        rpc: {
-            url: 'http://127.0.0.1:6888/',
-            method: 'POST',
-            auth: {
-                username: 'root',
-                password: 'pass'
-            },
+    version: '1.2.1',
+    coins: [
+        {
+            id: 'MN-01',
+            rpc: {
+                url: 'http://127.0.0.1:6888/',
+                method: 'POST',
+                auth: {
+                    username: 'root',
+                    password: 'pass'
+                },
+            }
+        },
+        {
+            id: 'MN-02',
+            rpc: {
+                url: 'http://127.0.0.1:6888/',
+                method: 'POST',
+                auth: {
+                    username: 'root',
+                    password: 'pass'
+                },
+            }
         }
-    }  
+
+    ]
 
 }
